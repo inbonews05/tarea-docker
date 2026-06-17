f697e8ebb14dfaa44ce5f1c71902f2e106e30825
GAEL <inbounpupas05@gmail.com>
2026-06-17 07:19:56 -0600

    feat: app.py, test_app.py, requirements.txt, Dockerfile

---
+++ app.py
@@ -0,0 +1,8 @@
+def sumar(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    resultado = sumar(5, 7)
+    print("====================================")
+    print(f"Ejecución del código: 5 + 7 = {resultado}")
+    print("====================================")
